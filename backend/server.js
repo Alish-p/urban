7158e71fddffe05a220bdc2ed5751ea9dcbb4c72
Alish Palasara <palasaraalis@gmail.com>
2022-05-30 11:19:38 +0530

    dummy

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use("/api/users", userRouter);
 app.use("/api/waitings", waitingRouter);
 
 // const __dirname = path.resolve();
-
+//
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
